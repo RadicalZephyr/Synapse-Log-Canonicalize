66b1fad0223bf2b4467bf844b65af9a336a6a83e
Geoff Shannon <earthlingzephyr@gmail.com>
2012-08-09 09:55:57 -0700

    Fleshing out of SynapseEventTest and SynapseEvent

--- src/main/java/org/sagebionetworks/canonicalize/SynapseEvent.java
+++ src/main/java/org/sagebionetworks/canonicalize/SynapseEvent.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.canonicalize;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.joda.time.DateTime;
@@ -9,15 +10,62 @@ import org.joda.time.format.DateTimeFormatter;
 
 public class SynapseEvent {
 
-	public static final DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss,SSS").withZone(DateTimeZone.UTC);
+	private static final DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss,SSS").withZone(DateTimeZone.UTC);
 
-	DateTime timeStamp;
-	String controller, methodName, latency;
-	Map<String, String> properties;
+	private DateTime timeStamp;
+	private String controller, methodName;
+
+	private int latency;
+	private Map<String, String> properties;
 
 	public SynapseEvent(String line) {
-		String[] parts = line.split("\\[.*?\\]");
+		// The first three splits are limited to 2 because we only want the first match.
+		// Ideally though, there shouldn't be more than one match for any of these patterns
+		String[] parts = line.split("\\[.*?\\] - ", 2);
 		timeStamp = dateFormatter.parseDateTime(parts[0].trim());
+
+		String[] controllerAndRest = parts[1].trim().split("/", 2);
+		controller = controllerAndRest[0];
+
+		String[] methodAndRest = controllerAndRest[1].trim().split("\\?", 2);
+		methodName = methodAndRest[0];
+
+		// Don't limit this split, because we don't know how many params we have
+		String[] params = methodAndRest[1].split("&");
+		this.properties = new HashMap<String, String>(params.length);
+
+		for (String param : params) {
+			String[] keyVal = param.split("=");
+			if (!keyVal[0].equalsIgnoreCase("latency")) {
+				properties.put(keyVal[0], keyVal[1]);
+			} else {
+				latency = Integer.parseInt(keyVal[1]);
+			}
+		}
+	}
+
+	public static DateTimeFormatter getDateformatter() {
+		return dateFormatter;
+	}
+
+	public DateTime getTimeStamp() {
+		return timeStamp;
+	}
+
+	public String getController() {
+		return controller;
+	}
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public int getLatency() {
+		return latency;
+	}
+
+	public Map<String, String> getProperties() {
+		return properties;
 	}
 
 }
--- src/test/java/org/sagebionetworks/canonicalize/SynapseEventTest.java
+++ src/test/java/org/sagebionetworks/canonicalize/SynapseEventTest.java
@@ -2,28 +2,59 @@ package org.sagebionetworks.canonicalize;
 
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.junit.*;
 
 public class SynapseEventTest {
 
 	@Test
-	public void testParseSynapseEvent() {
+	public void testParseSynapseEvent() throws UnsupportedEncodingException {
 		String timeStamp = "2012-08-06 18:36:22,961";
 
-		String line = timeStamp+" [TRACE] - "+"AccessRequirementController"+"/"+"getUnfulfilledAccessRequirement"+
-		"?latency=6&userId=Geoff.Shannon%40sagebase.org&entityId=syn114138&request=org.sagebionetworks.authutil.ModParamHttpServletRequest%405cba727";
+		String controller = "AccessRequirementController";
+		String method = "getUnfulfilledAccessRequirement";
 
+		StringBuilder builder = new StringBuilder(String.format(
+				"%s [TRACE] - %s/%s?", timeStamp, controller, method));
+
+		String latencyLabel = "latency";
+		int latencyValue = 6;
+		builder.append(String.format("%s=%d", latencyLabel, latencyValue));
+
+		Map<String, String> propMap = new HashMap<String, String>();
+		propMap.put("userId", "Geoff.Shannon@sagebase.org");
+		propMap.put("entityId", "syn114138");
+		propMap.put("request",
+				"org.sagebionetworks.authutil.ModParamHttpServletRequest@5cba727");
+
+		for (Entry<String, String> entry : propMap.entrySet()) {
+			builder.append("&");
+			builder.append(entry.getKey());
+			builder.append("=");
+			builder.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+		}
+
+		String line = builder.toString();
 		SynapseEvent event = new SynapseEvent(line);
 
-
-		DateTimeFormatter dateFormatter = SynapseEvent.dateFormatter;
-
+		DateTimeFormatter dateFormatter = SynapseEvent.getDateformatter();
 		DateTime time = dateFormatter.parseDateTime(timeStamp);
 
-		assertEquals(time, event.timeStamp);
+		assertEquals(time, event.getTimeStamp());
+		assertEquals(latencyValue, event.getLatency());
+		assertEquals(controller, event.getController());
+		assertEquals(method, event.getMethodName());
+
+		for (String key : propMap.keySet()) {
+			assertEquals(propMap.get(key), URLDecoder.decode(event.getProperties().get(key), "UTF-8"));
+		}
 	}
 }
