f1f1503178e8e59eb46cc9d45e741bf08db101ec
Geoff Shannon <earthlingzephyr@gmail.com>
2012-08-14 17:26:59 -0700

    Initial roughcut implementation
    Has the elements for the basic file-merge algorithm.  The algorithm
    should run in a constant amount of space too.

--- src/main/java/org/sagebionetworks/canonicalize/Canonicalizer.java
+++ src/main/java/org/sagebionetworks/canonicalize/Canonicalizer.java
@@ -1,5 +1,18 @@
 package org.sagebionetworks.canonicalize;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.sagebionetworks.logging.SynapseEvent;
+import org.sagebionetworks.logging.SynapseLoggingUtils;
+
 /**
  * Hello world!
  *
@@ -9,4 +22,59 @@ public class Canonicalizer {
     {
 
     }
+
+	public void collateLogs(List<File> list) {
+		ArrayList<BufferedReader> streamBuffers = new ArrayList<BufferedReader>();
+
+		for (File file : list) {
+			try {
+				streamBuffers.add(new BufferedReader(new FileReader(file)));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		boolean done = false;
+
+		SortedMap<SynapseEvent, BufferedReader> fileEventMap = new TreeMap<SynapseEvent, BufferedReader>();
+		for (BufferedReader stream : streamBuffers) {
+			try {
+				String line = stream.readLine();
+				SynapseEvent event = SynapseLoggingUtils.parseSynapseEvent(line);
+				fileEventMap.put(event, stream);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		while (!done) {
+			SynapseEvent firstKey = fileEventMap.firstKey();
+			BufferedReader rdr = fileEventMap.get(firstKey);
+			enhanceEvent(firstKey);
+			writeEvent(firstKey);
+			fileEventMap.remove(firstKey);
+
+			try {
+				String line = rdr.readLine();
+				SynapseEvent next = SynapseLoggingUtils.parseSynapseEvent(line);
+				fileEventMap.put(next, rdr);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				continue;
+			}
+		}
+	}
+
+	private void writeEvent(SynapseEvent firstKey) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void enhanceEvent(SynapseEvent firstKey) {
+		// TODO Auto-generated method stub
+
+	}
 }
--- src/test/java/org/sagebionetworks/canonicalize/CanonicalizerTest.java
+++ src/test/java/org/sagebionetworks/canonicalize/CanonicalizerTest.java
@@ -2,7 +2,11 @@ package org.sagebionetworks.canonicalize;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple Canonicalizer.
@@ -11,8 +15,15 @@ public class CanonicalizerTest {
 
 	Canonicalizer canon;
 
+	@Before
+	public void setup() {
+		canon = new Canonicalizer();
+	}
+
 	@Test
     public void testCollateLogs() {
+		List<File> list = new ArrayList<File>();
 
+		canon.collateLogs(list);
     }
 }
